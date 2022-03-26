0a5c4402112234dd6d2c659dd09f2a94e084e52b
Wanderer <wanderer.a@gmail.com>
2022-03-26 22:55:12 +0300

    Initial commit

---
+++ foobar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def foobar():
+    for i in range(1, 101):
+        printable = True
+        if i % 3 == 0:
+            printable = False
+            print('foo', end='')
+        if i % 5 == 0:
+            printable = False
+            print('bar', end='')
+        if i <= 100 and printable:
+            print(f'{i}', end='')
+        if i < 100:
+            print(', ', end='')
+
+
+if __name__ == '__main__':
+    foobar()
